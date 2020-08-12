0a9fae55482689327671208f20ec160968e85795
jinyaoMa <jinyao.ma@outlook.com>
2020-08-12 19:47:10 -0400

    deploy

---
+++ precache-manifest.ac40a4cd878c264785409a6c2a47dc31.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "31d7460c74de0f3c4afcb6ef0f8fe99e",
+    "url": "/Palette/index.html"
+  },
+  {
+    "revision": "daf7b56c12fab1fd76e2",
+    "url": "/Palette/static/css/main.d85b4b22.chunk.css"
+  },
+  {
+    "revision": "59a1e997c7099157a9b8",
+    "url": "/Palette/static/js/2.1cc6785c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Palette/static/js/2.1cc6785c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "daf7b56c12fab1fd76e2",
+    "url": "/Palette/static/js/main.34bf1dd1.chunk.js"
+  },
+  {
+    "revision": "b5de21db4ea8a1d5d28b",
+    "url": "/Palette/static/js/runtime-main.54dbeef8.js"
+  }
+]);
